d14eb12652ce7e14dc000e29409b57776caf3a1f
Aggelos Tselios <aggelostselios777@gmail.com>
2025-02-14 15:04:16 +0200

    dialog: Make use of `transient_for` for windows
    Plus provide an alternative function without asking for a parent window

--- src/dialog.rs
+++ src/dialog.rs
@@ -18,14 +18,34 @@
 /// You should have received a copy of the GNU General Public License  
 /// along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-use gtk::{prelude::*, Dialog as GtkDialog, Label, ResponseType};
+use gtk::{prelude::*, ApplicationWindow, Dialog as GtkDialog, Label, ResponseType};
 
 pub struct Dialog {
     obj: GtkDialog,
 }
 
 impl Dialog {
-    pub fn new(title: &str, msg: &str) -> Self {
+    pub fn new(title: &str, msg: &str, parent: &ApplicationWindow) -> Self {
+        let obj = GtkDialog::builder()
+            .title(title)
+            .modal(true)
+            .transient_for(parent)
+            .focus_visible(true)
+            .build();
+
+        let content = obj.content_area();
+        obj.add_button("Close", ResponseType::Ok);
+        obj.set_title(Some(title));
+        content.append(&Label::new(Some(msg)));
+
+        obj.connect_response(|dialog, _| {
+            dialog.close();
+        });
+
+        Self { obj }
+    }
+
+    pub fn new_without_parent(title: &str, msg: &str) -> Self {
         let obj = GtkDialog::builder()
             .title(title)
             .modal(true)
